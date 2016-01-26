e35751719f8646eb6c9da6b1c711ef160190eb96
John (Jack) Brown <jack@brownjohnf.com>
2016-01-25 20:16:43 -0800

    Add support for -h/-w resizing flags

--- main.go
+++ main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"image"
 	_ "image/gif"  // register GIF decoder
@@ -11,21 +12,35 @@ import (
 	"io"
 	"os"
 
+	"github.com/nfnt/resize"
 	_ "golang.org/x/image/webp"
 )
 
+var w = flag.Int("w", 0, "resize-to width")
+var h = flag.Int("h", 0, "resize-to height")
+
 func main() {
-	if err := toPNG(os.Stdin, os.Stdout); err != nil {
-		fmt.Fprintf(os.Stderr, "png: %v\n", err)
+	flag.Parse()
+
+	if err := toPNG(os.Stdin, os.Stdout, uint(*w), uint(*h)); err != nil {
+		fmt.Fprintf(os.Stderr, "topng: %v\n", err)
 		os.Exit(1)
 	}
 }
 
-func toPNG(in io.Reader, out io.Writer) error {
+func toPNG(in io.Reader, out io.Writer, width uint, height uint) error {
 	img, kind, err := image.Decode(in)
 	if err != nil {
 		return err
 	}
-	fmt.Fprintln(os.Stderr, "Input format =", kind)
+	fmt.Fprintln(os.Stderr, "Input format: ", kind)
+
+	if width > 0 || height > 0 {
+		fmt.Fprintln(os.Stderr, "resizing image")
+		newImg := resize.Resize(width, height, img, resize.Lanczos3)
+
+		return png.Encode(out, newImg)
+	}
+
 	return png.Encode(out, img)
 }
